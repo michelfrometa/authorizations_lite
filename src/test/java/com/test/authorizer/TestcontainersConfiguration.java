db54ab4a0c541cb508b96bb6b6e6ef04f06cfd40
Michel Frometa <michafrometa@gmail.com>
2024-10-19 06:57:19 -0300

    feat: Add Testcontainers configuration(#12)

--- src/test/java/com/test/authorizer/TestcontainersConfiguration.java
+++ src/test/java/com/test/authorizer/TestcontainersConfiguration.java
@@ -3,7 +3,6 @@ package com.test.authorizer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -12,14 +11,8 @@ class TestcontainersConfiguration {
 
 	@Bean
 	@ServiceConnection
-	MongoDBContainer mongoDbContainer() {
-		return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
-	}
-
-	@Bean
-	@ServiceConnection
 	MySQLContainer<?> mysqlContainer() {
-		return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+		return new MySQLContainer<>(DockerImageName.parse("mysql:5.7"));
 	}
 
 }
