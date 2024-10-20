6dff7e24de4cb7fb3a1082539193a2209f6989a1
Michel Frometa <michafrometa@gmail.com>
2024-10-20 04:23:37 -0300

    feat: Add Arquitectural Tests (#13)

---
+++ src/test/java/com/test/authorizer/architecture/CleanArchitectureTest.java
@@ -0,0 +1,32 @@
+package com.test.authorizer.architecture;
+
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+@AnalyzeClasses(packages = "com.test.authorizer", importOptions = ImportOption.DoNotIncludeTests.class)
+class CleanArchitectureTest {
+
+    static final String PRESENTATION_LAYER = "Presentation";
+    static final String USE_CASE_LAYER = "UseCase";
+    static final String DOMAIN_LAYER = "Domain";
+    static final String INFRASTRUCTURE_LAYER = "Infrastructure";
+
+    @ArchTest
+    static final ArchRule layer_dependencies_are_respected = layeredArchitecture()
+            .consideringAllDependencies()
+            .layer(PRESENTATION_LAYER).definedBy("com.test.authorizer.presentation..")
+            .layer(USE_CASE_LAYER).definedBy("com.test.authorizer.domain.usecase..")
+            .layer(DOMAIN_LAYER).definedBy("com.test.authorizer.domain..")
+            .layer(INFRASTRUCTURE_LAYER).definedBy("com.test.authorizer.infrastructure..")
+
+            .whereLayer(PRESENTATION_LAYER).mayNotBeAccessedByAnyLayer()
+            .whereLayer(USE_CASE_LAYER).mayOnlyBeAccessedByLayers(PRESENTATION_LAYER)
+            .whereLayer(INFRASTRUCTURE_LAYER).mayNotBeAccessedByAnyLayer()
+            .withOptionalLayers(true);
+
+
+}
---
+++ src/test/java/com/test/authorizer/architecture/DependenciesTest.java
@@ -0,0 +1,21 @@
+package com.test.authorizer.architecture;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+class DependenciesTest {
+
+    @Test
+    void doNotCallDeprecatedMethodsFromTheProject() {
+        JavaClasses importedClasses = new ClassFileImporter()
+                .importPackages("com.test.authorizer");
+        ArchRule rule = noClasses().should()
+                .dependOnClassesThat()
+                .areAnnotatedWith(Deprecated.class);
+        rule.check(importedClasses);
+    }
+}
