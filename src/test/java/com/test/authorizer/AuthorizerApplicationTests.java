b4f5817cafc0227a254eda7d22a0389220eec6b4
Michel Frometa <michafrometa@gmail.com>
2024-10-21 01:47:42 -0300

    test: Fix tests configuration and testcontainers visibility.

--- src/test/java/com/test/authorizer/AuthorizerApplicationTests.java
+++ src/test/java/com/test/authorizer/AuthorizerApplicationTests.java
@@ -1,10 +1,11 @@
 package com.test.authorizer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-import com.test.authorizer.config.TestcontainersConfiguration;
+import com.test.authorizer.configuration.TestcontainersConfiguration;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
@@ -12,6 +13,10 @@ class AuthorizerApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(AuthorizerApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
 }
