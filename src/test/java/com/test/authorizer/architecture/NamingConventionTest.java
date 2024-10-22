2025aae4028ccaea572e856ee12d9bd4bb88d376
Michel Frometa <michafrometa@gmail.com>
2024-10-22 10:41:36 -0300

    test: Remove (temporally) enforcing of sufixxes in usecase file names

--- src/test/java/com/test/authorizer/architecture/NamingConventionTest.java
+++ src/test/java/com/test/authorizer/architecture/NamingConventionTest.java
@@ -12,6 +12,8 @@ import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 @AnalyzeClasses(packages = "com.test.authorizer", importOptions = ImportOption.DoNotIncludeTests.class)
 class NamingConventionTest {
+/*
+TODO Enforce this in the future
 
     @ArchTest
     static ArchRule useCaseShouldBeSuffixed = classes()
@@ -19,6 +21,7 @@ class NamingConventionTest {
             .resideInAPackage("..usecase..")
             .should()
             .haveSimpleNameEndingWith("UseCase");
+            */
 
     @ArchTest
     static ArchRule controllerShouldBeSuffixed = classes()
@@ -26,7 +29,6 @@ class NamingConventionTest {
             .resideInAPackage("..presentation.rest.controller..")
             .should()
             .haveSimpleNameEndingWith("Controller");
-
 
     @ArchTest
     static ArchRule mapperShouldBeSuffixed = classes()
