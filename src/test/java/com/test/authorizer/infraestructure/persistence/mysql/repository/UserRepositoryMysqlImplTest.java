47b6d3ec643bb96f48452f77f12ffada5e44e6d9
Michel Frometa <michafrometa@gmail.com>
2024-10-22 22:19:01 -0300

    test: Add test for UserRepositoryMysqlImpl

---
+++ src/test/java/com/test/authorizer/infraestructure/persistence/mysql/repository/UserRepositoryMysqlImplTest.java
@@ -0,0 +1,81 @@
+package com.test.authorizer.infraestructure.persistence.mysql.repository;
+
+import com.test.authorizer.domain.model.User;
+import com.test.authorizer.infraestructure.persistence.mysql.mapper.IUserMySqlMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserRepositoryMysqlImplTest {
+
+    @Mock
+    private IUserRepositoryMysql userRepositoryMysql;
+
+    @Mock
+    private IUserMySqlMapper mapper;
+
+    @InjectMocks
+    private UserRepositoryMysqlImpl userRepositoryMysqlImpl;
+
+    private User user;
+    private com.test.authorizer.infraestructure.persistence.mysql.entity.User savedUser;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        savedUser = new com.test.authorizer.infraestructure.persistence.mysql.entity.User();
+
+    }
+
+    @Test
+    public void testSave() {
+        // Arrange
+        when(mapper.toPersistenceModel(eq(user))).thenReturn(savedUser);
+        when(userRepositoryMysql.save(eq(savedUser))).thenReturn(savedUser);
+        when(mapper.toEntity(eq(savedUser))).thenReturn(user);
+
+        // Act
+        User savedUser = userRepositoryMysqlImpl.save(user);
+
+        // Assert
+        assertNotNull(savedUser);
+        verify(userRepositoryMysql).save(any());
+    }
+
+    @Test
+    public void testSave_WithNullUser() {
+        // Arrange
+        User user = null;
+
+        // Act and Assert
+        try {
+            userRepositoryMysqlImpl.save(user);
+            assert false;
+        } catch (NullPointerException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void testSave_WithMapperReturningNull() {
+        // Arrange
+        User user = new User();
+        when(mapper.toPersistenceModel(user)).thenReturn(null);
+
+        // Act and Assert
+        assertEquals(userRepositoryMysqlImpl.save(user), null);
+    }
+
+}
+
