7dc0ed427290df09766807ba3d685229586efd13
Michel Frometa <michafrometa@gmail.com>
2024-10-23 03:29:58 -0300

    refactor: Fix deprecated code

--- src/test/java/com/test/authorizer/domain/validator/impl/ICardValidatorImplTest.java
+++ src/test/java/com/test/authorizer/domain/validator/impl/ICardValidatorImplTest.java
@@ -1,11 +1,10 @@
 package com.test.authorizer.domain.validator.impl;
 
 import com.test.authorizer.application.input.card.CreateCardDto;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,19 +13,15 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class ICardValidatorImplTest {
 
-    private ICardValidatorImpl iCardValidator;
-
     @Mock
     private CreateCardDto createCardDto;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.initMocks(this);
-        iCardValidator = new ICardValidatorImpl();
-    }
+    @InjectMocks
+    private ICardValidatorImpl iCardValidator;
 
     @Test
     void validate_validInput_returnsSameInput() {
+        // Fixme Implement othe tests
         // Arrange
         String expectedNumber = "1234567890";
         String expectedPassword = "password";
