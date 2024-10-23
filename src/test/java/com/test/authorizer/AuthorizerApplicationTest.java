05716ac7737511f80b62e0760d7dad3e0f2eac8b
Michel Frometa <michafrometa@gmail.com>
2024-10-23 03:09:45 -0300

    chore: Remove Testcontainers Support for Local Development

--- src/test/java/com/test/authorizer/AuthorizerApplicationTest.java
+++ src/test/java/com/test/authorizer/AuthorizerApplicationTest.java
@@ -1,11 +1,9 @@
 package com.test.authorizer;
 
+import com.test.authorizer.configuration.TestcontainersConfiguration;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-
-import com.test.authorizer.configuration.TestcontainersConfiguration;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
@@ -13,10 +11,6 @@ class AuthorizerApplicationTest {
 
 	@Test
 	void contextLoads() {
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.from(AuthorizerApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
 }
