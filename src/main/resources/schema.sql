1628aebb66dc809c6eb3e776d136d56959fbca8c
Michel Frometa <michafrometa@gmail.com>
2024-10-24 06:45:22 -0300

    refactor: Sql inits

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,13 @@
 insert into users (id, username, password)
-values (1, 'username', '$2a$12$V5tDICU8qTWEcuxts8x2g.JiJfnSpaPkfuwofB0WjYbY9IqJ3YwO.');
+values (1, 'username1', '$2a$12$RSRxtNgbwgm2ICq1abMEjuoVXKrNeLyj0Uei846u1n7OOBUjW0yNa');
+insert into users (id, username, password)
+values (2, 'username1', '$2a$12$RSRxtNgbwgm2ICq1abMEjuoVXKrNeLyj0Uei846u1n7OOBUjW0yNa');
+insert into users (id, username, password)
+values (3, 'username1', '$2a$12$RSRxtNgbwgm2ICq1abMEjuoVXKrNeLyj0Uei846u1n7OOBUjW0yNa');
 
 insert into cards (id, user_id, balance, number, password)
-values (1, 1, 20.5, 999999999, 'password');
+values (1, 1, 100, 999999999, 'password');
+insert into cards (id, user_id, balance, number, password)
+values (1, 2, 200, 999999998, 'password');
+insert into cards (id, user_id, balance, number, password)
+values (1, 3, 500, 999999997, 'password');
